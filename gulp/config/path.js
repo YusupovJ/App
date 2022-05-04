eebbde7ed9aa268ba137d27549e1eeb911980dc5
YusupovJ <jamshudanamana@gmail.com>
2022-05-04 09:42:27 +0500

    1v

---
+++ gulp/config/path.js
@@ -0,0 +1,33 @@
+import * as nodePath from "path";
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./${rootFolder}`;
+const srcFolder = "./src";
+
+export let path = {
+	build: {
+		images: `${buildFolder}/assets/img/`,
+		js: `${buildFolder}/scripts/`,
+		css: `${buildFolder}/styles/`,
+		html: `${buildFolder}/`,
+		fonts: `${buildFolder}/assets/fonts/`,
+	},
+	src: {
+		images: `${srcFolder}/assets/img/**/*.{png,jpg,webp,gif,jpeg}`,
+		js: `${srcFolder}/scripts/script.js`,
+		scss: `${srcFolder}/styles/style.scss`,
+		html: `${srcFolder}/*.html`,
+		svgicons: `${srcFolder}/assets/svg/*.svg`,
+	},
+	watch: {
+		images: `${srcFolder}/**/*.{png,jpg,webp,gif,jpeg,svg}`,
+		js: `${srcFolder}/**/*.js`,
+		scss: `${srcFolder}/**/*.scss`,
+		html: `${srcFolder}/**/*.html`,
+		svgicons: `${srcFolder}/**/*.svg`,
+	},
+	clean: buildFolder,
+	srcFolder: srcFolder,
+	buildFolder: buildFolder,
+	rootFolder: rootFolder,
+};
---
+++ src/scripts/modules/base.js
@@ -0,0 +1,15 @@
+/* Проверка на поддержку браузера формата webp */
+import { isWebP } from "./isWebP.js";
+isWebP();
+
+// /* IBG позволяет из тега в HTML img, сделать background свойством в css */
+// import { ibg } from "./ibg.js";
+// ibg();
+
+// /* Проверка на устройство пользователя */
+// import { isMobile } from "./isMobile.js";
+// if (isMobile.any()) {
+// 	document.body.classList.add("touch");
+// } else {
+// 	document.body.classList.add("pc");
+// }
